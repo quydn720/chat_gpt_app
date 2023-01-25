79e3ee98d987c234bf5b49d843ad27ceab3bbe5f
Quy Do Ngoc <quydn720@gmail.com>
2023-01-25 20:03:01 +0700

    init project

---
+++ android/app/src/main/kotlin/com/quydn720/chat_gpt_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.quydn720.chat_gpt_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ChatGptApp());
+}
+
+class Chat {
+  final bool isChatGpt;
+  final String text;
+
+  Chat({this.isChatGpt = false, required this.text});
+}
+
+class ChatGptApp extends StatefulWidget {
+  const ChatGptApp({super.key});
+
+  @override
+  State<ChatGptApp> createState() => _ChatGptAppState();
+}
+
+class _ChatGptAppState extends State<ChatGptApp> {
+  final _controller = TextEditingController();
+  final _scrollController = ScrollController();
+  final _conversations = <Chat>[];
+  var isQuestion = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ChatGPT App with Flutter',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('ChatGPT App')),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView(
+                  controller: _scrollController,
+                  children: _conversations.map(
+                    (e) {
+                      return Text(
+                        e.text,
+                        textAlign:
+                            e.isChatGpt ? TextAlign.start : TextAlign.end,
+                      );
+                    },
+                  ).toList(),
+                ),
+              ),
+              const Divider(),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(child: TextField(controller: _controller)),
+                  IconButton(
+                    onPressed: () {
+                      setState(
+                        () {
+                          isQuestion = !isQuestion;
+                          _scrollController.animateTo(
+                            _scrollController.position.maxScrollExtent,
+                            duration: const Duration(milliseconds: 100),
+                            curve: Curves.easeOut,
+                          );
+                          _conversations.add(
+                            Chat(text: _controller.text, isChatGpt: isQuestion),
+                          );
+                        },
+                      );
+                      debugPrint(_controller.text);
+                    },
+                    icon: const Icon(Icons.send),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
